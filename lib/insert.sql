d1c60de714cdde1443ff928df65358cde447169c
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-07-21 11:11:58 +0300

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 12, "M", "brown", "happy", 1),
+("Rowdy", 10, "M", "black", "playful", 1),
+("Tabitha", 6, "F", "blonde", "curious", 1),
+("Sergeant Brown", 14, "M", "brown", "grumpy", 1),
+("Melissa", 13, "F", "black", "gentle", 1),
+("Grinch", 11, "M", "brown", "mischievous", 1),
+("Wendy", 6, "F", "brown", "calm", 1),
+(NULL, 5, "F", "black", "shy", 1);
